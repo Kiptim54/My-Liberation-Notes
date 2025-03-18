e2e468174d185bf5af6721e3eeb4f74c232aa77b
Brenda Kiptim <brenda.kiptim@spherity.com>
2025-03-18 12:40:13 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mx-auto py-6">
+      <h1 className="text-5xl">My Liberation Notes</h1>
+      <p className="text-xl mt-4">A data story</p>
+    </div>
+  );
+}
+
+export default App;
