98d45f211879bfcaa7c1363ee48a1c86cca3da03
Kiptim54 <kiptim54@gmail.com>
2025-05-08 09:48:25 +0300

    added ratings to the sentiments line chart

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -5,9 +5,15 @@ type TData = {
   episode: number;
   sentiment: number;
 };
+
+type TRatings = {
+  episode: number;
+  rating: number;
+};
 // chart to analyse the emotional growth in the episodes using sentiment analysis
 export default function LineChart() {
   const [chartData, setChartData] = React.useState<TData[]>([]);
+  const [ratings, setRatings] = React.useState<TRatings[]>([]);
   const dimensions = React.useMemo(
     () => ({
       width: 800,
@@ -31,6 +37,20 @@ export default function LineChart() {
       });
       setChartData(processedData);
     });
+
+    // ratiings
+    d3.csv("/assets/episode-ratings.csv").then((data) => {
+      const processedData = data.map((item) => {
+        const episode = Number(item?.Episode);
+        const rating = item?.Ratings;
+
+        return {
+          episode: Number(episode) || 0,
+          rating: Number(rating) || 0,
+        };
+      });
+      setRatings(processedData);
+    });
   }, []);
 
   useEffect(() => {
@@ -43,6 +63,9 @@ export default function LineChart() {
     const minMaxEpisodes = d3.extent(chartData, (d) => d.episode) || [1, 16];
     const minMaxSentiment = d3.extent(chartData, (d) => d.sentiment) || [
       -30, 30,
+    ];
+    const minMaxEpisodesRatings = d3.extent(ratings, (d) => d.episode) || [
+      1, 16,
     ];
 
     // console.log({ minMaxEpisodes, minMaxSentiment });
@@ -64,9 +87,14 @@ export default function LineChart() {
       .range([
         dimensions.height - dimensions.margin.bottom,
         dimensions.margin.top,
-      ])
-      .nice();
-
+      ]);
+    const yScaleRatings = d3
+      .scaleLinear()
+      .domain(minMaxEpisodesRatings as [number, number])
+      .range([
+        dimensions.height - dimensions.margin.bottom,
+        dimensions.margin.top,
+      ]);
     const lineGenerator = d3
       .line<TData>()
       .x((d) => xScale(xAccessor(d)))
@@ -138,11 +166,40 @@ export default function LineChart() {
         .attr("stroke-width", 2);
     }
 
+    const yAxisRatings = wrapper
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${dimensions.width - dimensions.margin.right}, 0)`
+      )
+      .call(d3.axisRight(yScaleRatings).ticks(5));
+
+    yAxisRatings.selectAll("path").attr("stroke", "black"); // For the axis line
+    yAxisRatings
+      .selectAll("text") // Select all tick labels
+      .attr("fill", "black") // Set the text color
+      .style("font-size", "12px"); // Optional: Set font size
+
+    // Draw the ratings line
+    const lineGeneratorRatings = d3
+      .line<TRatings>()
+      .x((d) => xScale(d.episode))
+      .y((d) => yScaleRatings(d.rating))
+      .curve(d3.curveCardinal.tension(0.5)); // Smooth curve
+    // draw the ratings line
+
+    wrapper
+      .append("path")
+      .attr("d", lineGeneratorRatings(ratings))
+      .attr("fill", "none")
+      .attr("stroke", "#8E734B")
+      .attr("stroke-width", 3)
+      .attr("class", "animated-line-ratings");
     // Draw the x axis label
     wrapper
       .append("text")
-      .attr("x", dimensions.width - dimensions.margin.right - 20)
-      .attr("y", dimensions.height - dimensions.margin.bottom - 20)
+      .attr("x", dimensions.width / 2 - dimensions.margin.right)
+      .attr("y", dimensions.height + 20)
       .attr("text-anchor", "middle")
       .text("Episodes")
       .style("font-size", "16px")
@@ -156,6 +213,17 @@ export default function LineChart() {
       .attr("text-anchor", "middle")
       // .attr("transform", `translate(${0}, ${dimensions.height / 2}) rotate(0)`)
       .text("Sentiments")
+      .style("font-size", "16px")
+      .style("font-weight", "bold");
+
+    // Draw the y axis label for ratings
+    wrapper
+      .append("text")
+      .attr("x", dimensions.width - dimensions.margin.right + 10)
+      .attr("y", dimensions.margin.top - 10)
+      .attr("text-anchor", "middle")
+      // .attr("transform", `translate(${0}, ${dimensions.height / 2}) rotate(0)`)
+      .text("Ratings")
       .style("font-size", "16px")
       .style("font-weight", "bold");
     // Tooltip creation
@@ -173,12 +241,27 @@ export default function LineChart() {
       .style("display", "none")
       .style("z-index", "10");
 
+    // ratings tooltip
+    const ratingsTooltip = d3
+      .select("#wrapper")
+      .append("div")
+      .attr("id", "ratings-tooltip")
+      .style("position", "absolute")
+      .style("background", "white")
+      .style("padding", "6px 10px")
+      .style("border", "1px solid #ccc")
+      .style("border-radius", "4px")
+      .style("pointer-events", "none")
+      .style("font-size", "12px")
+      .style("display", "none")
+      .style("z-index", "10");
     // Add circles for each data point
     wrapper
-      .selectAll("circle")
+      .selectAll(".sentiment-circle")
       .data(chartData)
       .enter()
       .append("circle")
+      .attr("class", "sentiment-circle")
       .attr("cx", (d) => xScale(d.episode))
       .attr("cy", (d) => yScale(d.sentiment))
       .attr("r", 5)
@@ -230,22 +313,99 @@ export default function LineChart() {
           .style("display", "block")
           .html(
             `<strong>Episode:</strong> ${d?.episode}<br/>
-             <strong>Sentiment:</strong> ${d.sentiment.toFixed(2)}`
+             <strong>Sentiment:</strong> ${d.sentiment.toFixed(2)}
+             <strong>Ratings:</strong> ${ratings[d.episode - 1]?.rating.toFixed(
+               2
+             )} <br/>
+             `
           );
       })
       .on("mouseout", function () {
         tooltip.style("display", "none");
       });
 
+    // // Add circles for each data point for ratings
+    wrapper
+      .selectAll(".ratings-circle")
+      .data(ratings)
+      .enter()
+      .append("circle")
+      .attr("class", "ratings-circle")
+      .attr("cx", (d) => xScale(d.episode))
+      .attr("cy", (d) => yScaleRatings(d.rating))
+      .attr("r", 5)
+      .attr("fill", "#8E734B")
+      // at the moment I cannot sfind a color for the negatives. I will play around with that on Figma
+
+      .on("mouseover", function (_event, d) {
+        ratingsTooltip
+          .style("display", "block")
+          .html(
+            `<div class="text-black"><strong >Episode:</strong> ${
+              d.episode
+            }<br/><strong">Ratings:</strong> ${d.rating.toFixed(2)}</div>`
+          );
+      })
+      .on("mousemove", function (event, d) {
+        const [mouseX, mouseY] = d3.pointer(event);
+        // get episode
+
+        // const bisect = d3.bisector((d: TData) => d.episode).right;
+        // const index = bisect(chartData, mouseX);
+        // const closestDataPoint = chartData[index];
+
+        const tooltipWidth = 120;
+        const tooltipHeight = 50;
+
+        // Calculate initial position
+        let left = mouseX + 10;
+        let top = mouseY - tooltipHeight - 10;
+
+        // Adjust if tooltip goes beyond the right edge
+        if (left + tooltipWidth > dimensions.width - 10) {
+          left = mouseX - tooltipWidth - 20;
+        }
+
+        // Adjust if tooltip goes above the top edge
+        if (top < 0) {
+          top = mouseY + 40;
+        }
+
+        ratingsTooltip
+          .style("left", `${left}px`)
+          .style("top", `${top}px`)
+          .style("display", "block")
+          .html(
+            `<strong>Episode:</strong> ${d?.episode}<br/>
+           <strong>Ratings:</strong> ${d.rating.toFixed(2)}
+           <strong>Sentiment:</strong> ${chartData[
+             d.episode - 1
+           ]?.sentiment.toFixed(2)} <br/>
+           `
+          );
+      })
+      .on("mouseout", function () {
+        ratingsTooltip.style("display", "none");
+      });
+
     const path = d3.select(".animated-line").node() as SVGPathElement;
+    const pathRatings = d3
+      .select(".animated-line-ratings")
+      .node() as SVGPathElement;
     if (!path) return;
+    if (!pathRatings) return;
 
     const pathLength = path.getTotalLength();
+    const pathLengthRatings = pathRatings.getTotalLength();
 
     // Initial state: hidden
     d3.select(path)
       .attr("stroke-dasharray", pathLength)
       .attr("stroke-dashoffset", pathLength);
+
+    d3.select(pathRatings)
+      .attr("stroke-dasharray", pathLengthRatings)
+      .attr("stroke-dashoffset", pathLengthRatings);
 
     const handleScroll = () => {
       const scrollTop = window.scrollY;
@@ -254,7 +414,7 @@ export default function LineChart() {
       if (!wrapper) return;
 
       const { top, height } = wrapper.getBoundingClientRect();
-      const start = top + scrollTop - windowHeight * 0.9; // start animating when half the wrapper is in view
+      const start = top + scrollTop - windowHeight * 0.5; // start animating when half the wrapper is in view
       const end = start + height;
 
       const scrollPercent = Math.min(
@@ -263,12 +423,17 @@ export default function LineChart() {
       );
 
       const drawLength = pathLength * scrollPercent;
+      const drawLengthRatings = pathLengthRatings * scrollPercent;
+      d3.select(pathRatings).attr(
+        "stroke-dashoffset",
+        pathLengthRatings - drawLengthRatings
+      );
       d3.select(path).attr("stroke-dashoffset", pathLength - drawLength);
     };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [chartData, dimensions]);
+  }, [chartData, dimensions, ratings]);
 
   return <div id="wrapper" className="relative  mx-auto overflow-hidden"></div>;
 }
--- src/components/EpisodesSentiments.tsx
+++ src/components/EpisodesSentiments.tsx
@@ -5,21 +5,43 @@ export default function EpisodesSentiments() {
     <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen bg-secondaryLight">
       <LineChart />
       <div className="bg-secondaryLight flex flex-col  text-black justify-center p-6 gap-4">
-        <p className="font-outfit text-xl  font-bold">
-          This is the sentiment progression through the episodes
+        <p className=" text-xl font-extrabold ">
+          Episode{" "}
+          <span className="relative">
+            {" "}
+            <span className="relative z-10 font-semibold ">
+              Sentiments
+            </span>{" "}
+            <span className="bg-secondary2 inset-0 absolute -z-0 w-[80%] opacity-60 mx-auto"></span>{" "}
+          </span>{" "}
+          vs{" "}
+          <span className="relative">
+            {" "}
+            <span className="relative z-10 font-semibold ">Ratings</span>
+            <span className="bg-secondary3 inset-0 absolute -z-0 w-[80%] opacity-80 mx-auto"></span>
+          </span>
+          :
         </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui labore
-          facere enim culpa non quae dignissimos! Unde reprehenderit inventore
-          impedit, natus, odit error eveniet debitis eligendi maiores sequi
-          ducimus nulla perferendis ipsa vel laudantium similique reiciendis
-          maxime atque esse totam ipsam excepturi quae non deserunt! Quo numquam
-          aspernatur fuga aliquam blanditiis debitis possimus ratione totam
-          libero sit voluptate in iste non nesciunt rem culpa quia perspiciatis
-          iure id nostrum, commodi excepturi expedita. Dignissimos nisi porro
-          accusantium esse ea ut quae? Cupiditate iure enim ab est, accusantium
-          corporis veritatis aliquam molestiae sapiente sint a eius et animi
-          accusamus magni beatae eligendi?
+        <p className="font-outfit text-normal">
+          Most stories have a tried and tested formula, and kdramas are
+          notorious yet loved for this. From the plot setting, to the rising
+          incident, the climax and finally the resolution, these narratives hold
+          us captives as we wait for our dopamine hit just to start the cycle
+          again.
+          <br />
+          <br />
+          Yet in My Liberation Notes, we could say that, for the most part,
+          nothing happens. People wake up, commute to work, commute back home,
+          eat and talk and repeat the same day over and over again. For some
+          viewers, this was unbearably dull, but to others, this was calming and
+          a true representation of daily life. The exhaustion of the cycle of
+          life. For all its nothingness, it begs the question, "Isn't that sad,
+          isn't that us?"
+          <br />
+          <br />
+          With most characters burdened by life, the sentiments of the episodes
+          are mostly negative. Once in a while, there is hope, only for it to be
+          dashed and blown out by life.
         </p>
       </div>
     </div>
